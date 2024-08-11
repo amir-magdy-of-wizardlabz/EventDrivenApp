3845fd2c5de667e6d90bacb111cf7139634fe1ca
amir-magdy-of-wizardlabz <131914221+amir-magdy-of-wizardlabz@users.noreply.github.com>
2024-08-12 00:06:29 +0300

    mail sender notification service

--- NotificationService/NotificationService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ NotificationService/NotificationService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Core.Interfaces;
 using NotificationService.Infrastructure.Data;
+using NotificationService.Infrastructure.Utilities;
 
 namespace NotificationService.Infrastructure.Extensions
 {
@@ -11,6 +13,7 @@ namespace NotificationService.Infrastructure.Extensions
         {
             AddDataInfrastructureService(services, configuration);
             AddMessagingInfrastructureServices(services, configuration);
+            AddNotificationInfrastructureServices(services, configuration);
             return services;
         }
 
@@ -22,6 +25,11 @@ namespace NotificationService.Infrastructure.Extensions
 
             services.AddHostedService<MigrationHostedService>();
 
+        }
+        private static IServiceCollection AddNotificationInfrastructureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<INotificationService, EmailSender>();
+            return services;
         }
 
         private static IServiceCollection AddMessagingInfrastructureServices(IServiceCollection services, IConfiguration configuration)
